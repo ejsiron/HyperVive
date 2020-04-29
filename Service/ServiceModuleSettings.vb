c9d92daabfa030419316c089d7ded0a52037d3ce
Eric Siron <ejsiron@users.noreply.github.com>
2020-04-28 23:29:38 -0500

    Switch registry base to settings region

--- Service/ServiceModuleSettings.vb
+++ Service/ServiceModuleSettings.vb
@@ -1,5 +1,11 @@
 ﻿Partial Public Class ModuleController
 	Private Const ServiceRegistryPathTemplate As String = "SYSTEM\CurrentControlSet\Services\{0}"
+	Private ReadOnly Property ServiceRegistryPath As String
+		Get
+			Static CombinedPath As String = String.Format(ServiceRegistryPathTemplate, Service.ServiceName)
+			Return CombinedPath
+		End Get
+	End Property
 	Private ServiceRegistryRootPath As String
 	Private DebugMode As Boolean = False
 	Private DebugModeRegistryController As RegistryController
